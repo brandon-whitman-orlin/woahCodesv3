4d8550cbe98b585539db49a8343f8365af441783
brandon-whitman-orlin <brandonwhitmanorlin@gmail.com>
2023-06-11 02:08:59 -0400

    Safari Test 14

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let introducing = document.getElementById("introducing");
     // const defaultSnapList = [0, -36, -72.32, -108.48, -144.8];
     const smallSnapList = [5.1, -21.5, -48.1, -74.7, -101.3];
-    const safariSnapList = [5.3, -21.5, -48.1, -74.7, -107.4];
+    const safariSnapList = [5.6, -21.5, -48.1, -74.7, -107.4];
     let snapList = smallSnapList;
 
     let safari = false;
