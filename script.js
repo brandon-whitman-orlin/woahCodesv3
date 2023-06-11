d80657d767885b7a7d477bc2fa7f4d611d3d6ff1
brandon-whitman-orlin <brandonwhitmanorlin@gmail.com>
2023-06-11 01:07:39 -0400

    Sliding Intro

--- script.js
+++ script.js
@@ -53,3 +53,67 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
     }
 })
+
+// Handle Clicking and Dragging for the Intro Text
+document.addEventListener("DOMContentLoaded", (event) => {
+    const elements = document.querySelectorAll('.clickDrag');
+    const vScrollContainer = document.querySelector('.vScroll');
+    let introducing = document.getElementById("introducing");
+    const snapList = [0, -36, -72.32, -108.48, -144.8];
+
+    let isDragging = false;
+    let initialY = 0;
+    let yOffset = 0;
+    const minTop = 0;
+    const maxTop = -9.05 * parseFloat(getComputedStyle(vScrollContainer).fontSize);
+
+    elements.forEach(function(element) {
+        element.addEventListener('mousedown', function() {
+            element.classList.add('dragging');
+        });
+
+        element.addEventListener('mouseup', function() {
+            element.classList.remove('dragging');
+        });
+    });
+
+    function dragStart(e) {
+        isDragging = true;
+        initialY = e.clientY;
+        yOffset = vScrollContainer.offsetTop;
+        vScrollContainer.classList.add('dragging');
+    }
+
+    function drag(e) {
+        if (isDragging) {
+            const deltaY = e.clientY - initialY;
+            let newTop = Math.max(Math.min(yOffset + deltaY, minTop), maxTop);
+            introducing.setAttribute("data-state", snapList.indexOf(findClosestNumber(newTop, snapList)))
+            vScrollContainer.style.top = `${findClosestNumber(newTop, snapList)}px`;
+        }
+    }
+
+    function dragEnd() {
+        if (isDragging) {
+            isDragging = false;
+            vScrollContainer.classList.remove('dragging');
+            console.log(vScrollContainer.style.top);
+        }
+    }
+
+    vScrollContainer.addEventListener('mousedown', dragStart);
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', dragEnd);
+
+    document.addEventListener('selectstart', (e) => {
+        if (isDragging) {
+            e.preventDefault();
+        }
+    });
+
+    function findClosestNumber(inputNumber, numbers) {
+        return numbers.reduce((closest, current) => {
+            return Math.abs(inputNumber - current) < Math.abs(inputNumber - closest) ? current : closest;
+        });
+    }
+});
