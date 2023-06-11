1fddaec57be2c06283f59dd5b157cc116ea7e7e3
brandon-whitman-orlin <brandonwhitmanorlin@gmail.com>
2023-06-11 02:17:40 -0400

    S 21

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let introducing = document.getElementById("introducing");
     // const defaultSnapList = [0, -36, -72.32, -108.48, -144.8];
     const smallSnapList = [5.1, -21.5, -48.1, -74.7, -101.3];
-    const safariSnapList = [4.8, -21.5, -50, -74.7, -107.4];
+    const safariSnapList = [4.8, -21.5, -52, -74.7, -107.4];
     let snapList = smallSnapList;
 
     let safari = false;
